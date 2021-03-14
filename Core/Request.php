98cfe8fe89b8b648e961f6d2e14d66b8b659606d
jackhugh <24830612+jackhugh@users.noreply.github.com>
2021-03-14 05:18:29 +0000

    updated readme

--- Core/Request.php
+++ Core/Request.php
@@ -17,6 +17,7 @@ class Request {
 	) {
 		$this->method = $method;
 		$this->url = $url;
+		$this->params = (object) [];
 		$this->query = (object) $_GET;
 		$this->post = (object) $_POST;
 		$this->cookies = (object) $_COOKIE;
